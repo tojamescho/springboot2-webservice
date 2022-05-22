288c3b6ff2c80a401cf372775af6d92c4e8c8444
tojamescho <to.james.cho@gmail.com>
2022-05-22 22:42:33 +0900

    * Add Controller & Dto and related test code - Add HelloController, HelloResponseDto and related test code - Change gradle version

---
+++ src/main/java/com/james/cho/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,14 @@
+package com.james.cho.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+
+}
